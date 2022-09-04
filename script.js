61838b93a7551bc13cda03693e5074dd4a7fb7ad
최준호 <mukdol1127@naver.com>
2022-09-04 18:22:47 +0900

    feat: +enter&AC button run

--- script.js
+++ script.js
@@ -1,6 +1,25 @@
+const calculator = document.querySelector('.calculator');
 const result = document.querySelector('.result');
 const contents = document.querySelector('.contents');
-const calculator = document.querySelector('.calculator');
+const operator = document.querySelector('.operator')
+const calculation = document.querySelector('.calculation');
+
+
+function calculate(n1, operator, n2) {
+  let result = 0;
+
+  if (operator === '+') {
+    result = Number(n1) + Number(n2);
+  } else if (operator === '-') {
+    result = Number(n1) - Number(n2);
+  } else if  (operator === '×') {
+    result = Number(n1) * Number(n2);
+  } else if (operator === '÷') {
+    result = Number(n1) / Number(n2);
+  }
+   return String(result);
+}
+let firstNum, operatoration, previousKey, previousNum;
 
 
 contents.addEventListener('click', function(event) {
@@ -15,12 +34,36 @@ contents.addEventListener('click', function(event) {
   if (target.matches('button')) {
     if (action === 'number') {
       console.log(buttonContent);
-      if (result.textContent === '0') {
+      if (result.textContent === '0' || previousKey === 'operator') {
+        
         result.textContent = buttonContent;
       } else {
         result.textContent += buttonContent;
       }
+      // 연산자를 누르면 초기화시키고 다시 입력이 되야한다
+     previousKey = 'number';
+    }
 
+    if (action === 'operator') {
+      firstNum = result.textContent;
+      operatoration = buttonContent;
+      previousKey = 'operator';
+      console.log(previousKey);
+      console.log(operatoration);
+    }
+
+    if (action === 'clear_ac') {
+      result.textContent = '0';
+      firstNum = undefined;
+      operatoration = undefined;
+      
+      
+    }
+
+    if (action === 'calculation') {
+      previousNum = result.textContent;
+     result.textContent = calculate(firstNum, operatoration, previousNum)
+     console.log('결과');
     }
     
   }
